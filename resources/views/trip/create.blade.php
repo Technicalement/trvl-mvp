fe47e6c542e590b6bd9c6583cb11e4cb8734becb
codecaiine <akabrouyannickn@gmail.com>
2023-02-15 23:27:47 +0000

    Fix input

--- resources/views/trip/create.blade.php
+++ resources/views/trip/create.blade.php
@@ -44,7 +44,13 @@
                   <div class="absolute w-5 h-5 bg-[#8EB8A1] rounded-full -left-2.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                   <div class="px-5 pb-5">
                      <label class="text-sm font-medium">Arrival:</label>
-                     <input  name="arrival_city"  placeholder="City, Country" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400">
+                     <select name="reason" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400 " required="required" name="reason" id="reason">
+                        <option value="None" disabled selected>City, Country ?</option>
+                        <option value="London">London</option>
+                        <option value="Bangkok">Bangkok</option>
+                        <option value="Sidney">Sidney</option>
+                     </select>
+                     
                      <label class="text-sm font-medium">Departure (optional):</label>
                      <input name="departure_city" placeholder="Add where you’re leaving from if it’s different from the arrival destination" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"> 
                      <div class="flex">
@@ -58,8 +64,8 @@
                         </div>
                      </div>
                      <label class="text-sm font-medium">What brings you here?</label>
-                     <select name="reason" class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required="required" name="reason" id="reason">
-                        <option value="none">What brings you to this place?</option>
+                     <select name="reason" class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400 " required="required" name="reason" id="reason">
+                        <option value="None" disabled selected>What brings you to this place?</option>
                         <option value="vaccation">vaccation</option>
                         <option value="work">work</option>
                         <option value="business">Business</option>
